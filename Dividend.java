4c72bd4c9b7c460c5c5060666d811860854a8d67
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-28 17:37:08 +0530

    Add files via upload

---
+++ Dividend.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Dividend 
+{
+	public static void main(String[] args) 
+	{
+Scanner sc=new Scanner(System.in);
+System.out.println("MAIN METHOD STARTED");
+int s=3000;
+int s2=(2000*100/(10*(10/100)));
+int s3=s-s2;
+System.out.println("SHARES HE HAS NOW"+s2);
+	
+System.out.println("number of more shares he has"+s3);
+}
+}
