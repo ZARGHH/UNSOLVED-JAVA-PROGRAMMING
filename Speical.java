ac599e400c9f5ca5a7ab27ad3c5ae04063fe602e
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-29 17:39:25 +0530

    Add files via upload

---
+++ Speical.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Speical 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("MAIN METHOD STARTED");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+
+		int sum=b+c;
+		int pro=b*c;
+		int total=sum+pro;
+		System.out.println(" THE TOTAL IS"+total);
+		System.out.println("THE Number is"+a);
+System.out.println("MAIN METHOD ENDED");
+
+
+	}
+}
